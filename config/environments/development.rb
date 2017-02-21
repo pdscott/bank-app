021dbafe4632ba757922c78b433aab67c6226e9a
pscott <github@preston.fastmail.fm>
2017-02-21 18:44:35 -0500

    Extra Credit should now be working.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,11 +45,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp@gmail.com',
     port: '587',
-    # domain: 'example.com',
+    #domain: 'gmail.com',
     authentication: 'plain',
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: 'pdscott2@ncsu.edu',
+    password: 'N9u6DMGyhgiN'
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
